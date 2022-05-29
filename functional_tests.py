437e123f610dc273721c27e7482d298c5c6988c8
Clay123456789 <56254386+Clay123456789@users.noreply.github.com>
2022-05-30 02:21:18 +0800

    Redirect after POST,and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -19,17 +19,27 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do',self.browser.title)
         header_text=self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
+
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox=self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-        self.assertIn(
-            '2:Use peacock feathers to make a fly',[row.text for row in rows]
-        )
+        # The page updates again,and now shows both items on her list
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+
         self.fail('Finish the test!')
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
