4b76cfdf31efb54e663bf3a243ee5a0b22b6206b
Clay123456789 <56254386+Clay123456789@users.noreply.github.com>
2022-05-30 22:17:21 +0800

    new URL,view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.test import TestCase
 from lists.models import Item
 
@@ -23,13 +24,6 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(),0)
         AssertionError: 1!=0
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response=self.client.get('/')
-        self.assertIn('itemey 1',response.content.decode())
-        self.assertIn('itemey 2',response.content.decode())
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -50,6 +44,9 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text,'Item the second' )
 
 class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response=self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response,'list.html')
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
