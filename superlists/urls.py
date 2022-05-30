182f455455606594d9e4a66114e0c62f9b5d8109
Clay123456789 <56254386+Clay123456789@users.noreply.github.com>
2022-05-30 21:48:27 +0800

    Add a new user in system

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,5 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from lists import views
 urlpatterns = [
-    path('', views.home_page,name="home"),
+    # url(r'^$',views.home_page,name='home'),
+    # url(r'^lists/the-only-list-in-the-world/$',views.view_list,name='view_list'),
+    path('',views.home_page,name='home'),
+    path('lists/the-only-list-in-the-world/', views.view_list,name="view_list"),
 ]
