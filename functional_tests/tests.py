02f152ffef729bdf53e098b388a1ed91c2676f14
Clay123456789 <56254386+Clay123456789@users.noreply.github.com>
2022-05-31 10:44:52 +0800

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 
 MAX_WAIT=10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self) :
         self.browser =webdriver.Chrome()
     def tearDown(self) :
+        self.browser.refresh()
         self.browser.quit()
 
     def wait_for_row_in_list_table(self,row_text):
